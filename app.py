4fd2b953de7ce41f6eb9d6f5aeaf574ad20e6afb
jaspinderrkaur1611-ux <jaspinderrkaur1611@gmail.com>
2025-08-29 13:45:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LogisticRegression
 import pandas as pd
 
 
+
 # Load data
 url = "iris.csv"
 iris = pd.read_csv(url)
@@ -43,6 +44,8 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port=int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0',port=port,debug=False)
+
 
 
